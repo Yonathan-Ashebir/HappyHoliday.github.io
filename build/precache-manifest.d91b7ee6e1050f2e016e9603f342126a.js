4514aea87a79b91fe653bd9d1eb56996bf3a18b1
Yonathan Ashebir <yonatha12345678910@gmail.com>
2023-01-07 23:00:00 +0300

    Removed build dir from gitignore for using it in github pages

---
+++ build/precache-manifest.d91b7ee6e1050f2e016e9603f342126a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6dcb81b6b1b044b6bd0",
+    "url": "/static/css/main.1c23af6e.chunk.css"
+  },
+  {
+    "revision": "c6dcb81b6b1b044b6bd0",
+    "url": "/static/js/main.c6dcb81b.chunk.js"
+  },
+  {
+    "revision": "d18151b26033b92b7cd2",
+    "url": "/static/js/1.d18151b2.chunk.js"
+  },
+  {
+    "revision": "e651d4058b6f092b0d10",
+    "url": "/static/js/2.e651d405.chunk.js"
+  },
+  {
+    "revision": "53206e605dd86847922f",
+    "url": "/static/js/runtime~main.53206e60.js"
+  },
+  {
+    "revision": "2cd62167659ce54cddd88d5d37297f6a",
+    "url": "/index.html"
+  }
+];
